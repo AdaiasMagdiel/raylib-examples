8c61e4031acb0637985ac4b64fd0ef7806cbfa0a
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-02-02 18:20:14 -0300

    style: Change close brackets style

--- examples/gradient_rect/main.c
+++ examples/gradient_rect/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 
-float map(float value, float start1, float stop1, float start2, float stop2) {
+float map(float value, float start1, float stop1, float start2, float stop2)
+{
     float newval = (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
 
     return newval;
